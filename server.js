318969611fa3e57890ffd6a9f5d96d0ab453034d
Julieta Andrea Lopez <julieta.lopez@claro.com.ar>
2023-08-28 10:09:51 -0300

    Se agrega server de tipo express

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const server = express();
+
+server.use(express.static('html')) //esto hace que se le diga a express usa tal plugins //archivo static son los html, css
+server.get("/personas", function(req, resp){
+    const mensaje = {msj: "Hola soy resp"}; 
+    console.log("El resp es: ", mensaje);
+    //resp.send(mensaje);   //send es una funcion del objeto resp que permite enviar la respuesta que quiero // ACA devuelve texto plano
+    resp.json(mensaje);
+});
+
+
+server.listen(3000, function(){
+    console.log("El server está activo en el puerto 3000")
+}); //puerto que escucha el server // el error de consola EADDRINUSE significa que ya está ocupado el puerto //ctrol+c para parar el server
