36e6dc351ffbf70ff81d00a9943bf1acac87f5e2
Julieta Andrea Lopez <julieta.lopez@claro.com.ar>
2023-09-01 18:19:50 -0300

    Conexion con backend

--- html/main.js
+++ html/main.js
@@ -1,13 +1,30 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
 
-const server = http.createServer((request, response) => {
-    const html = fs.readFileSync('index.html');
-    response.writeHeader(200, {"content-Type":"text/html"});
-    response.write(html);
-    response.end();
-});
+// const server = http.createServer((request, response) => {
+//     const html = fs.readFileSync('index.html');
+//     response.writeHeader(200, {"content-Type":"text/html"});
+//     response.write(html);
+//     response.end();
+// });
 
-server.listen(8080, () => {
-    console.log('servidor escuchando en http://localhost:' + 8080);
-});
+// server.listen(8080, () => {
+//     console.log('servidor escuchando en http://localhost:' + 8080);
+// });
+
+function boton(){
+    //alert("Me hiciste click")
+    msj()
+}
+
+async function msj(){
+    const resp = await fetch("/mensaje")
+    const mensaje = await resp.json(); 
+    console.log(mensaje)
+    const elemento = document.getElementById("mensaje")
+    console.log(elemento)
+    elemento.innerHTML = mensaje.mensaje;
+    elemento.style.color = "green"
+}
+
+//msj()
--- server.js
+++ server.js
@@ -3,13 +3,17 @@ import express from "express";
 const server = express();
 
 server.use(express.static('html')) //esto hace que se le diga a express usa tal plugins //archivo static son los html, css
-server.get("/personas", function(req, resp){
-    const mensaje = {msj: "Hola soy resp"}; 
-    console.log("El resp es: ", mensaje);
-    //resp.send(mensaje);   //send es una funcion del objeto resp que permite enviar la respuesta que quiero // ACA devuelve texto plano
-    resp.json(mensaje);
-});
+// server.get("/personas", function(req, resp){
+//     const mensaje = {msj: "Hola soy resp"}; 
+//     console.log("El resp es: ", mensaje);
+//     //resp.send(mensaje);   //send es una funcion del objeto resp que permite enviar la respuesta que quiero // ACA devuelve texto plano
+//     resp.json(mensaje);
+// });
 
+server.get("/mensaje", function(req, res){
+    const mensaje = {mensaje: "Bienvenido a este nuevo mundo"};
+    res.json(mensaje);
+});
 
 server.listen(3000, function(){
     console.log("El server está activo en el puerto 3000")
